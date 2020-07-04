dc1adb3f593943461af108bbf8f00c76762410e1
Ganeshk750 <ganesh9ot@gmail.com>
2020-07-04 22:41:00 +0530

    #adding FlatMap for IntegerList

---
+++ Java8Code/src/com/ganesh/faltmap/FlatMapForIntegrList.java
@@ -0,0 +1,29 @@
+package com.ganesh.faltmap;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlatMapForIntegrList {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("========FlatMap For Integer Lists============");
+		
+		//List<List<Integer>> -> Stream -> FlatMap -> Stream -> Collectors.toList() -> List<Integer>
+
+	   List<List<Integer>> listOfList = Arrays.asList(
+			      Arrays.asList(1, 2, 3, 4),
+		          Arrays.asList(5, 6, 7, 8),
+		          Arrays.asList(9, 10, 11, 12));
+	   System.out.println(listOfList);//[[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]
+	   
+	   List<Integer> list = listOfList.stream()
+			                   .flatMap(newList -> newList.stream())
+			                   .collect(Collectors.toList());
+	   
+	   System.out.println(list);//[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+	   
+	}
+
+}
